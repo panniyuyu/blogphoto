da4ce8c569136cea298fbaf1b8ad0de2df666cd0
panniyuyu <sbsbjs@qq.com>
2019-05-12 20:21:11 +0800

    add photos

--- tool.py
+++ tool.py
@@ -129,6 +129,7 @@ def handle_photo():
             list_info[-1]['arr']['text'].append(info)
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
+	#print list_info
     final_dict = {"list": list_info}
     with open("/usr/local/blog/myblog/themes/next/source/lib/album/data.json","w") as fp:
         json.dump(final_dict, fp)
